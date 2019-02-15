aec9124e689c15d193a9846eab17e5cf7772bf68
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 20:39:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,11 +11,11 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60
+	if speed > 60 ?
 	  return true
-	elsif speed < 40
+	elsif speed < 40 ?
 	  return true
-	else ?
+	else
 	  return false
 	end
 end
